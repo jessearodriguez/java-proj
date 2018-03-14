ac65eff6fc3a75e728ef78f31c2ae85c888ece3a
Aertex <Aertex@users.noreply.github.com>
2018-03-13 20:28:35 -0700

    save file + new sample input

--- src/main.java
+++ src/main.java
@@ -30,6 +30,7 @@ public class main extends JFrame {
 	/**
 	 * Launch the application.
 	 */
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -58,12 +59,11 @@ public class main extends JFrame {
 		contentPane.add(toolBar, "cell 0 0,grow");
 		
 		JButton btnNewButton = new JButton("Open");
-		
-		
-		
+
 		toolBar.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Save");
+		
 		toolBar.add(btnNewButton_1);
 		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Left justification");
@@ -78,41 +78,55 @@ public class main extends JFrame {
 		JScrollPane scrollPane = new JScrollPane();
 		contentPane.add(scrollPane, "cell 0 1,grow");
 		
-		JTextArea textArea = new JTextArea();
+		JTextArea textArea = new JTextArea();// top text box
 		textArea.setEditable(false);
 		scrollPane.setViewportView(textArea);
 		
 		JScrollPane scrollPane_1 = new JScrollPane();
 		contentPane.add(scrollPane_1, "cell 0 2,grow");
 		
-		JTextArea textArea_1 = new JTextArea();
+		JTextArea textArea_1 = new JTextArea(); //bottom text box
 		textArea_1.setEditable(false);
 		scrollPane_1.setViewportView(textArea_1);
+		
 
+		
+		final JFileChooser filepicker= new JFileChooser();
+		
 		btnNewButton.addActionListener(new ActionListener() //open button interrupt
 		{
-			JFileChooser filepicker;
+			
 			File textfile;
 			String filename;
 			FileReader fread;
 			BufferedReader buffreader;
+			String text;
+			
+			StringBuilder builder = new StringBuilder();
+			
 			public void actionPerformed(ActionEvent e) // file selection block
 			{
 
-				filepicker = new JFileChooser();
+				
 				filepicker.showOpenDialog(null);
 				
-				if(!(filepicker.getSelectedFile()==null)) //stops null poiner exeption on cancle
+				if(!(filepicker.getSelectedFile()==null)) //stops null poiner exeption on cancel
 				{
 					textfile = filepicker.getSelectedFile();
 					filename = textfile.getAbsolutePath();
-					
+					text = ""; //sets text to empty for new read
+					builder.setLength(0);
 					try 
 					{
 						fread = new FileReader(filename);
-						buffreader = new BufferedReader(fread);
-						textArea.read(buffreader, null);
+						buffreader = new BufferedReader(fread); //need a method here to read the formatted text into textarea.read
+					
+						while((text = buffreader.readLine()) != null) builder.append(text).append("\n");
+						text = builder.toString();
+						textArea.setText(text);
+						
 						buffreader.close();
+						fread.close();
 						textArea.requestFocus();
 					}
 					catch(Exception exeption1)
@@ -120,7 +134,42 @@ public class main extends JFrame {
 						JOptionPane.showMessageDialog(null, "Error in reading file, exeption:" + exeption1);
 					}
 				}
+				
 			}
+		});
+		
+		
+		
+		btnNewButton_1.addActionListener(new ActionListener()  //save file interupt
+		{
+			int id; //to check if cancled/accepted
+			
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				
+				id = filepicker.showSaveDialog(null);
+				
+				PrintWriter output;
+				
+				if(id == 0) //accepted 
+				{
+					
+					try 
+					{
+						output = new PrintWriter(filepicker.getSelectedFile());
+						output.write(textArea.getText()); //reads text from textarea, set in the other interupt
+						//System.out.print(textArea.getText()); // outputs correctly in console, but dosnt save correctly in .txt file
+						output.close();
+					}
+					catch(Exception exeption1)
+					{
+						JOptionPane.showMessageDialog(null, "Error in reading file, exeption:" + exeption1);
+					}
+					
+					
+				}
+			}
+			
 		});
 	}
 }
